5b192f77b28fcbd5560ac37494cb9699157bcc52
Visakh47 <visakhbobby47@gmail.com>
2021-05-26 19:32:32 +0530

    test

--- app.py
+++ app.py
@@ -300,6 +300,153 @@ def main():
                 st.error('There seems to be some error 🤔 , please try again later :( ')
 
 
+mapping_df = load_mapping()
+
+rename_mapping = {
+    'date': 'Date',
+    'min_age_limit': 'Minimum Age Limit',
+    'available_capacity': 'Available Capacity',
+    'vaccine': 'Vaccine',
+    'pincode': 'Pincode',
+    'name': 'Hospital Name',
+    'state_name' : 'State',
+    'district_name' : 'District',
+    'block_name': 'Block Name',
+    'fee_type' : 'Fees'
+    }
+
+st.title('CoWIN Vaccination Slot Availability')
+st.info('The CoWIN APIs are geo-fenced so sometimes you may not see an output! Please try after sometime ')
+
+valid_states = list(np.unique(mapping_df["state_name"].values))
+
+left_column_1, center_column_1, right_column_1 = st.beta_columns(3)
+with left_column_1:
+    numdays = st.slider('Select Date Range', 0, 100, 3)
+
+with center_column_1:
+    state_inp = st.selectbox('Select State', [""] + valid_states)
+    if state_inp != "":
+        mapping_df = filter_column(mapping_df, "state_name", state_inp)
+
+
+mapping_dict = pd.Series(mapping_df["district id"].values,
+                         index = mapping_df["district name"].values).to_dict()
+
+# numdays = st.sidebar.slider('Select Date Range', 0, 100, 10)
+unique_districts = list(mapping_df["district name"].unique())
+unique_districts.sort()
+with right_column_1:
+    dist_inp = st.selectbox('Select District', unique_districts)
+
+DIST_ID = mapping_dict[dist_inp]
+
+base = datetime.datetime.today()
+date_list = [base + datetime.timedelta(days=x) for x in range(numdays)]
+date_str = [x.strftime("%d-%m-%Y") for x in date_list]
+
+temp_user_agent = UserAgent()
+browser_header = {'User-Agent': temp_user_agent.random}
+
+final_df = None
+for INP_DATE in date_str:
+    URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={}&date={}".format(DIST_ID, INP_DATE)
+    response = requests.get(URL, headers=browser_header)
+    if (response.ok) and ('centers' in json.loads(response.text)):
+        resp_json = json.loads(response.text)['centers']
+        if resp_json is not None:
+            df = pd.DataFrame(resp_json)
+            if len(df):
+                df = df.explode("sessions")
+                df['min_age_limit'] = df.sessions.apply(lambda x: x['min_age_limit'])
+                df['vaccine'] = df.sessions.apply(lambda x: x['vaccine'])
+                df['available_capacity'] = df.sessions.apply(lambda x: x['available_capacity'])
+                df['date'] = df.sessions.apply(lambda x: x['date'])
+                df = df[["date", "available_capacity", "vaccine", "min_age_limit", "pincode", "name", "state_name", "district_name", "block_name", "fee_type"]]
+                if final_df is not None:
+                    final_df = pd.concat([final_df, df])
+                else:
+                    final_df = deepcopy(df)
+        else:
+            st.error("No rows in the data Extracted from the API")
+#     else:
+#         st.error("Invalid response")
+
+if (final_df is not None) and (len(final_df)):
+    final_df.drop_duplicates(inplace=True)
+    final_df.rename(columns=rename_mapping, inplace=True)
+
+    left_column_2, center_column_2, right_column_2, right_column_2a,  right_column_2b = st.beta_columns(5)
+    with left_column_2:
+        valid_pincodes = list(np.unique(final_df["Pincode"].values))
+        pincode_inp = st.selectbox('Select Pincode', [""] + valid_pincodes)
+        if pincode_inp != "":
+            final_df = filter_column(final_df, "Pincode", pincode_inp)
+
+    with center_column_2:
+        valid_age = [18, 45]
+        age_inp = st.selectbox('Select Minimum Age', [""] + valid_age)
+        if age_inp != "":
+            final_df = filter_column(final_df, "Minimum Age Limit", age_inp)
+
+    with right_column_2:
+        valid_payments = ["Free", "Paid"]
+        pay_inp = st.selectbox('Select Free or Paid', [""] + valid_payments)
+        if pay_inp != "":
+            final_df = filter_column(final_df, "Fees", pay_inp)
+
+    with right_column_2a:
+        valid_capacity = ["Available"]
+        cap_inp = st.selectbox('Select Availablilty', [""] + valid_capacity)
+        if cap_inp != "":
+            final_df = filter_capacity(final_df, "Available Capacity", 0)
+
+    with right_column_2b:
+        valid_vaccines = ["COVISHIELD", "COVAXIN"]
+        vaccine_inp = st.selectbox('Select Vaccine', [""] + valid_vaccines)
+        if vaccine_inp != "":
+            final_df = filter_column(final_df, "Vaccine", vaccine_inp)
+
+    table = deepcopy(final_df)
+    table.reset_index(inplace=True, drop=True)
+    st.table(table)
+else:
+    st.error("Unable to fetch data currently, please try after sometime")
+
+# footer="""<style>
+# a:link , a:visited{
+# color: blue;
+# background-color: transparent;
+# text-decoration: underline;
+# }
+
+# a:hover,  a:active {
+# color: red;
+# background-color: transparent;
+# text-decoration: underline;
+# }
+
+# .footer {
+# position: fixed;
+# left: 0;
+# bottom: 0;
+# width: 100%;
+# background-color: white;
+# color: black;
+# text-align: center;
+# }
+# </style>
+# <div class="footer">
+# <p>Developed with ❤ by <a style='display: block; text-align: center;' href="https://github.com/bhattbhavesh91" target="_blank">Bhavesh Bhatt</a></p>
+# </div>
+# """
+# st.markdown(footer,unsafe_allow_html=True)
+# st.markdown("_- Bhavesh Bhatt_")
+
+pageviews=Pageviews()
+pageviews.append('dummy')
+pg_views = len(pageviews)
+
 
     
     if option == 'Admin Page':
